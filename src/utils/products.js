2848148079657d8fbeb253c17fbb319569a748ab
Ary Cordeiro <ary_cordeiro@outlook.com>
2025-09-22 08:33:38 -0300

    Create products.js

---
+++ src/utils/products.js
@@ -0,0 +1,38 @@
+export const products = [
+  {
+    id: 1,
+    name: "Cadeira de Tambor",
+    price: "79,80",
+    image: require("../../assets/produtos/cadeira.png"),
+  },
+  {
+    id: 2,
+    name: "Mesa de Pneu",
+    price: "49,95",
+    image: require("../../assets/produtos/mesa.png"),
+  },
+  {
+    id: 3,
+    name: "Vasos Pet",
+    price: "19,90",
+    image: require("../../assets/produtos/vaso.png"),
+  },
+  {
+    id: 4,
+    name: "Sofá de Pallet",
+    price: "120,50",
+    image: require("../../assets/produtos/sofa.png"),
+  },
+  {
+    id: 5,
+    name: "Porta Canetas",
+    price: "8,90",
+    image: require("../../assets/produtos/porta.png"),
+  },
+  {
+    id: 6,
+    name: "Banco de Pneu",
+    price: "75,25",
+    image: require("../../assets/produtos/banco.png"),
+  },
+];
